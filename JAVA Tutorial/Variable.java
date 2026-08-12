b9dfdc3532943d2da331a39958763095b925630e
KG48 <karangavhane1213@gmail.com>
2026-08-12 17:53:56 +0530

    Add Java variable Examples

---
+++ JAVA Tutorial/Variable.java
@@ -0,0 +1,17 @@
+
+public class Variable {
+    public static void main(String[] args) {
+
+        int age = 20;                   // int, float  = 4 bytes
+        String name = "KG";               // byte = 1 byte
+        double height = 183.0;           // double, long = 8 bytes 
+        char grade = 'A';               // char, short = 2 bytes
+        boolean isStudent = true;       // not specified
+
+        System.out.println(age);
+        System.out.println(name);
+        System.out.println(height);
+        System.out.println(grade);
+        System.out.println(isStudent);
+    }
+}
