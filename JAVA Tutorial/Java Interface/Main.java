67641a5df17ed883c8a374e26e0265589fcdef9b
KG48 <karangavhane1213@gmail.com>
2026-09-09 11:28:22 +0530

    java interfaces basic code

---
+++ JAVA Tutorial/Java Interface/Main.java
@@ -0,0 +1,25 @@
+interface Animal {
+    void sound();
+    void eat();
+}
+
+class Dog implements Animal {
+
+    public void sound() {
+        System.out.println("Dog barks");
+    }
+
+    public void eat() {
+        System.out.println("Dog eats food");
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+
+        Dog d = new Dog();
+
+        d.sound();
+        d.eat();
+    }
+}
